92c49eca51f66737380c0b7b41e0fcbe27a47987
Anton <zditovets55@gmail.com>
2021-04-21 16:26:00 +0300

    update foundBookDTO

--- LibraryService.API.Aplication/Queries/BookQueries/GetAllBookQuery.cs
+++ LibraryService.API.Aplication/Queries/BookQueries/GetAllBookQuery.cs
@@ -53,23 +53,12 @@ namespace LibraryService.API.Application.Queries.BookQueries
                 AmountPage = book.AmountPage,
                 Title = book.Title,
                 Year = book.Year,
-                Author =
-                {
-                    Id = book.AuthorId.Value,
-                    Name = author.Name,
-                    Surname = author.Surname,
-                    Patronymic = author.Patronymic
-                },
-                Genre =
-                {
-                    Id = book.GenreId.Value,
-                    Name = genre.Name
-                },
-                Publisher =
-                {
-                    Id = book.PublisherId.Value,
-                    Name = publisher.Name
-                }
+                AuthorName = author.Name,
+                AuthorSurname = author.Surname,
+                AuthorPatronymic = author.Patronymic,
+                Genre = genre.Name,
+                Publisher = publisher.Name
+
             };
         }
 
